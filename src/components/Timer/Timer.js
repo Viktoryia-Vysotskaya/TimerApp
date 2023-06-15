01a58ab28bea62336595a14f0768ed5971b323aa
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-06-15 11:11:33 +0200

    Update type of Button and update Timer

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,14 +1,24 @@
 import styles from "./Timer.module.scss";
 
 const Timer = props => {
-    return(
-        <div className={styles.timer}>
-            <span>{("0" + Math.floor((props.time / 360000) % 60)).slice(-2)}&nbsp;:&nbsp;</span>
-            <span>{("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}&nbsp;:&nbsp;</span>
-            <span>{("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}&nbsp;:&nbsp;</span>
-            <span>{("0" + ((props.time) % 1000)).slice(-3)}&nbsp;</span>        
-        </div>
-    );
+  const hours = Math.floor((props.time / 3600000) % 24);
+  const minutes = Math.floor((props.time / 60000) % 60);
+  const seconds = Math.floor((props.time / 1000) % 60);
+  const milliseconds = props.time % 1000;
+
+  const formattedHours = ("0" + hours).slice(-2);
+  const formattedMinutes = ("0" + minutes).slice(-2);
+  const formattedSeconds = ("0" + seconds).slice(-2);
+  const formattedMilliseconds = ("0" + milliseconds).slice(-3);
+
+  return (
+    <div className={styles.timer}>
+      <span>{formattedHours}&nbsp;:&nbsp;</span>
+      <span>{formattedMinutes}&nbsp;:&nbsp;</span>
+      <span>{formattedSeconds}&nbsp;:&nbsp;</span>
+      <span>{formattedMilliseconds}&nbsp;</span>
+    </div>
+  );
 };
 
 export default Timer;
